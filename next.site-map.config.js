faf3ef7f2cac912dbe2f4ba9a54f45e2b7f47169
e <motusamotusrblx@gmail.com>
2025-08-23 13:04:27 +0800

    some stuff

---
+++ next.site-map.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://explorelombok.netlify.app", // nanti ganti ke domain hostinger kalau sudah
+    generateRobotsTxt: true,
+};
